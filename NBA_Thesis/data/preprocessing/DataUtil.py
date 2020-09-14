bdb77eb79b4ba98bf5be8dcdd128529e093e1c7d
dkStephanos <stephanos@goldmail.etsu.edu>
2020-09-14 12:59:05 -0400

    Added DataUtil methods to get game event/build player data dictionary

--- NBA_Thesis/data/preprocessing/DataUtil.py
+++ NBA_Thesis/data/preprocessing/DataUtil.py
@@ -11,3 +11,30 @@ class DataUtil:
         game_df = pd.read_json(path)
         
         return game_df
+
+    @staticmethod
+    def load_event_by_num(game_df, event_num):
+        return game_df['events'].iloc[event_num]
+
+    @staticmethod
+    def get_players_data(game_event):
+        # A dict containing home players data	
+        home = game_event["home"]	
+        # A dict containig visiting players data	
+        visitor = game_event["visitor"]
+        # creates the players list with the home players	
+        players = home["players"]	
+        # Then add on the visiting players	
+        players.extend(visitor["players"])	
+
+        # initialize new dictionary	
+        players_dict = {}	
+
+        # Add the values we want for the players (name and jersey number)
+        for player in players:	
+            players_dict[player['playerid']] = [player["firstname"]+" "+player["lastname"], player["jersey"]]	
+        
+        # Add an entry for the ball
+        players_dict.update({-1: ['ball', np.nan]})	
+
+        return players_dict
--- NBA_Thesis/data/preprocessing/parseData.py
+++ NBA_Thesis/data/preprocessing/parseData.py
@@ -8,35 +8,16 @@ from IPython.display import IFrame
 
 from VisualizationUtil import VisualizationUtil as VisUtil
 from FeatureUtil import FeatureUtil
+from DataUtil import DataUtil
 
-game_df = pd.read_json(r"C:\Users\Stephanos\Documents\Dev\NBAThesis\NBA_Thesis\NBA_Thesis\static\data\game_raw_data\11.19.2015.GSW.at.LAC\0021500177.json")	
+game_df = DataUtil.load_game_df(r"C:\Users\Stephanos\Documents\Dev\NBAThesis\NBA_Thesis\NBA_Thesis\static\data\game_raw_data\11.19.2015.GSW.at.LAC\0021500177.json")
 
-curr_event = game_df['events'].iloc[222]	
+curr_event = DataUtil.load_event_by_num(game_df, 222)	
 
-# A dict containing home players data	
-home = curr_event["home"]	
-# A dict containig visiting players data	
-visitor = curr_event["visitor"]	
+players_dict = DataUtil.get_players_data(curr_event)
+
 # A list containing each moment	
 moments = curr_event["moments"]	
-# Column labels	
-headers = ["team_id", "player_id", "x_loc", "y_loc", 	
-           "radius", "moment", "game_clock", "shot_clock"]	
-
-# creates the players list with the home players	
-players = home["players"]	
-# Then add on the visiting players	
-players.extend(visitor["players"])	
-
-# initialize new dictionary	
-id_dict = {}	
-
-# Add the values we want	
-for player in players:	
-    id_dict[player['playerid']] = [player["firstname"]+" "+player["lastname"],	
-                                   player["jersey"]]	
-
-id_dict.update({-1: ['ball', np.nan]})	
 
 # Initialize our new list	
 player_moments = []	
@@ -50,10 +31,10 @@ for moment in moments:
         player.extend((moments.index(moment), moment[2], moment[3]))	
         player_moments.append(player)	
 
-df = pd.DataFrame(player_moments, columns=headers)	
+df = pd.DataFrame(player_moments, columns=DataUtil.HEADERS)	
 
-df["player_name"] = df.player_id.map(lambda x: id_dict[x][0])	
-df["player_jersey"] = df.player_id.map(lambda x: id_dict[x][1])	
+df["player_name"] = df.player_id.map(lambda x: players_dict[x][0])	
+df["player_jersey"] = df.player_id.map(lambda x: players_dict[x][1])	
 
 # get Curry's movements	
 curry = df[df.player_name=="Stephen Curry"]	
