127ba484f839e4f54922a3ebe246195b55f1c18c
dkStephanos <stephanos@goldmail.etsu.edu>
2020-10-25 16:06:06 -0400

    added game_name to game model

--- data/models.py
+++ data/models.py
@@ -1,20 +1,5 @@
 from django.db import models	
 
-class Constant(models.Model):
-    NORMALIZATION_COEF = 7
-    PLAYER_CIRCLE_SIZE = 12 / NORMALIZATION_COEF
-    INTERVAL = 10
-    DIFF = 6
-    X_MIN = 0
-    X_MAX = 100
-    Y_MIN = 0
-    Y_MAX = 50
-    COL_WIDTH = 0.3
-    SCALE = 1.65
-    FONTSIZE = 6
-    X_CENTER = X_MAX / 2 - DIFF / 1.5 + 0.10
-    Y_CENTER = Y_MAX - DIFF / 1.5 - 0.35
-
 class Ball(models.Model):
     x = models.FloatField() 
     y = models.FloatField()
@@ -47,6 +32,7 @@ class Event(models.Model):
 
 class Game(models.Model):
     game_id = models.IntegerField()
+    game_name = models.CharField(max_length=30)
     home_team = models.ForeignKey(Team, related_name="home_team", on_delete=models.SET_NULL, null=True)
     guest_team = models.ForeignKey(Team, related_name="guest_team", on_delete=models.SET_NULL, null=True)
     events = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True)
