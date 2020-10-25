9cc9e390c4066f2d89582c2d2579e2c20c01b627
dkStephanos <stephanos@goldmail.etsu.edu>
2020-10-25 16:15:02 -0400

    Splitting old parseData to playground file to test code, making parseData dedicated to parsing json file and instantiating Django models

--- data/preprocessing/playground.py
+++ data/preprocessing/playground.py
@@ -51,3 +51,36 @@ event_df = pd.DataFrame(player_moments, columns=DataUtil.HEADERS)
 
 event_df["player_name"] = event_df.player_id.map(lambda x: players_dict[x][0])	
 event_df["player_jersey"] = event_df.player_id.map(lambda x: players_dict[x][1])	
+
+# get Curry's movements	
+curry = event_df[event_df.player_name=="Stephen Curry"]
+
+#all_player_loc = DataUtil.get_all_player_position_data(event_df)
+#print(all_player_loc.head())
+
+#print(curry.head())
+
+VisUtil.plot_player_movement(curry)
+
+#dist = FeatureUtil.travel_dist(curry)
+#print(dist)
+
+#all_dist = FeatureUtil.travel_dist_all(event_df)
+#print(all_dist)
+
+#average_speed = FeatureUtil.average_speed(event_df, curry)
+#print(average_speed)
+
+#average_speed_all = FeatureUtil.average_speed_all(event_df)
+#print(average_speed_all)
+
+#ball_distances = FeatureUtil.distance_between_ball_and_players(event_df)
+#ball_dist_df = DataUtil.convert_labled_series_to_df('player_name', 'ball_distances', ball_distances)
+#min_dist_df = DataUtil.get_labled_mins_from_df(ball_dist_df, 'dist_from_ball')
+
+#min_dist_df.loc[min_dist_df['dist_from_ball'] > 3.0, 'player_name'] = pd.NA
+
+#print(min_dist_df.shape)
+#print(min_dist_df)
+
+#min_dist_df.to_csv('static/data/features/ball_handler.csv')
