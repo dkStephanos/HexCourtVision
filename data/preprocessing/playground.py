a4778f6c932913d79c9f98c7faf094fba3a7a45e
dkStephanos <stephanos@goldmail.etsu.edu>
2020-12-17 14:31:30 -0500

    Added more games to constants, changed playground to process all games

--- data/preprocessing/playground.py
+++ data/preprocessing/playground.py
@@ -1,87 +1,55 @@
 import pandas as pd	
 import numpy as np	
+import sys
 
 import matplotlib.pyplot as plt	
 import seaborn as sns	
 
-from IPython.display import IFrame	
-
-from utilities.GraphUtil import GraphUtil
 from utilities.FeatureUtil import FeatureUtil
 from utilities.DataUtil import DataUtil
+from utilities.ConstantsUtil import ConstantsUtil
 
-NORMALIZATION_COEF = 7
-PLAYER_CIRCLE_SIZE = 12 / NORMALIZATION_COEF
-INTERVAL = 10
-DIFF = 6
-X_MIN = 0
-X_MAX = 100
-Y_MIN = 0
-Y_MAX = 50
-COL_WIDTH = 0.3
-SCALE = 1.65
-FONTSIZE = 6
-X_CENTER = X_MAX / 2 - DIFF / 1.5 + 0.10
-Y_CENTER = Y_MAX - DIFF / 1.5 - 0.35
+for game in ConstantsUtil.games:
+    game_df = DataUtil.load_game_df(ConstantsUtil.games[game]['raw_data'])
+    annotation_df = DataUtil.load_annotation_df(ConstantsUtil.games[game]['events'])  
+    print(f"Loaded game {game}")
 
-game_df = DataUtil.load_game_df(r"C:\Users\Stephanos\Documents\Dev\NBAThesis\NBA_Thesis\static\data\game_raw_data\12.25.2015.LAC.at.LAL\0021500440.json")
-print(game_df["events"][200]["eventId"])
+    teams_data = DataUtil.get_teams_data(game_df)
+    players_data = DataUtil.get_players_data(game_df)
+    players_dict = DataUtil.get_players_dict(game_df)
+    print("Extracted team/player data")
 
-game_df = DataUtil.load_game_df(r"C:\Users\Stephanos\Documents\Dev\NBAThesis\NBA_Thesis\static\data\game_raw_data\12.11.2015.GSW.at.BOS\0021500336.json")
-print(game_df.head())
-curr_event = DataUtil.load_game_event_by_num(game_df, "201")	
+    annotation_df = DataUtil.trim_annotation_rows(annotation_df, ConstantsUtil.games[game]['bad_events'])
+    annotation_df = FeatureUtil.determine_possession(annotation_df, teams_data)
+    annotation_df = DataUtil.generate_event_ids(annotation_df)
+    annotation_df = DataUtil.trim_annotation_cols(annotation_df)
+    combined_event_df = DataUtil.combine_game_and_annotation_events(game_df, annotation_df)
+    # Get direction for each play, and remove moments occuring on the other half of the court
+    combined_event_df = FeatureUtil.determine_directionality(combined_event_df)
+    combined_event_df = DataUtil.trim_moments_by_directionality(combined_event_df)
+    print("Processed and combined events")
 
-players_dict = DataUtil.get_players_data(curr_event)
+    print("Starting Candidate Extraction\n")
+    all_candidates = []
+    succesful = 0
+    failed = 0
+    for index, event in combined_event_df.iterrows():
+        try:
+            moments_df = DataUtil.get_moments_from_event(event)
+            if len(moments_df) > 0:
+                event_passes = FeatureUtil.get_passess_for_event(moments_df, event["possession"], players_data)
+                dribble_handoff_candidates = FeatureUtil.get_dribble_handoff_candidates(combined_event_df, moments_df, event_passes, ConstantsUtil.games[game]['moment_range'], players_dict)
+                all_candidates += dribble_handoff_candidates
+            else:
+                print("No moments for event: " + str(event['EVENTNUM']))
+            succesful += 1
+        except:
+            print("Issue at index: " + str(event['EVENTNUM']), sys.exc_info())
+            failed += 1
 
-# A list containing each moment	
-moments = curr_event["moments"]	
+    final_candidates = DataUtil.remove_duplicate_candidates(all_candidates)
+    print("\nNumber of candidates parsed: " + str(len(final_candidates)) + "\nSuccessful events: " + str(succesful) + "\nFailed events: " + str(failed) + "\nPercent Successful: " + str(round(succesful/(failed + succesful), 2)))
 
-# Initialize our new list	
-player_moments = []	
-
-for moment in moments:	
-    # For each player/ball in the list found within each moment	
-    for player in moment[5]:	
-        # Add additional information to each player/ball	
-        # This info includes the index of each moment, the game clock	
-        # and shot clock values for each moment	
-        player.extend((moments.index(moment), moment[2], moment[3]))	
-        player_moments.append(player)	
-
-event_df = pd.DataFrame(player_moments, columns=DataUtil.HEADERS)	
-
-event_df["player_name"] = event_df.player_id.map(lambda x: players_dict[x][0])	
-event_df["player_jersey"] = event_df.player_id.map(lambda x: players_dict[x][1])	
-
-# get Curry's movements	
-curry = event_df[event_df.player_name=="Stephen Curry"]
-
-#all_player_loc = DataUtil.get_all_player_position_data(event_df)
-#print(all_player_loc.head())
-
-#print(curry.head())
-
-GraphUtil.plot_player_movement(curry)
-
-#dist = FeatureUtil.travel_dist(curry)
-#print(dist)
-
-#all_dist = FeatureUtil.travel_dist_all(event_df)
-#print(all_dist)
-
-#average_speed = FeatureUtil.average_speed(event_df, curry)
-#print(average_speed)
-
-#average_speed_all = FeatureUtil.average_speed_all(event_df)
-#print(average_speed_all)
-
-#ball_distances = FeatureUtil.distance_between_ball_and_players(event_df)
-#ball_dist_df = DataUtil.convert_labled_series_to_df('player_name', 'ball_distances', ball_distances)
-#min_dist_df = DataUtil.get_labled_mins_from_df(ball_dist_df, 'dist_from_ball')
-
-#min_dist_df.loc[min_dist_df['dist_from_ball'] > 3.0, 'player_name'] = pd.NA
-
-#print(min_dist_df.shape)
-#print(min_dist_df)
-
-#min_dist_df.to_csv('static/data/features/ball_handler.csv')
+    candidate_df = pd.DataFrame(final_candidates)
+    candidate_df.to_csv(f'static/data/test/candidates-{game}.csv')
+    print("Saving to csv...\n")
