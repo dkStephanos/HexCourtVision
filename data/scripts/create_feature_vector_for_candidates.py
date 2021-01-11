7c61f44b9a6c2d115d702c45c8a05d58168b39c6
dkStephanos <stephanos@goldmail.etsu.edu>
2021-01-11 12:41:10 -0500

    Now fetching all candidates for a single game from db

--- data/scripts/create_feature_vector_for_candidates.py
+++ data/scripts/create_feature_vector_for_candidates.py
@@ -3,11 +3,25 @@ import pandas as pd
 import easygui
 import sys
 
-from utilities.DataUtil import DataUtil
-from utilities.FeatureUtil import FeatureUtil
-from utilities.ConstantsUtil import ConstantsUtil
+from data.preprocessing.utilities.DataUtil import DataUtil
+from data.preprocessing.utilities.FeatureUtil import FeatureUtil
+from data.preprocessing.utilities.ConstantsUtil import ConstantsUtil
 
-from ..models import Game
+from data.models import Game
+from data.models import Team
+from data.models import Player
+from data.models import Event
+from data.models import Moment
+from data.models import Candidate
 
+def run():
+    print(Game.objects.all())
 
-print(Game.objects.all()[0])
+    candidates = Candidate.objects.none()
+    events = Event.objects.filter(game=Game.objects.all()[0])
+    for event in events:
+        next_candidates = Candidate.objects.filter(event=event)
+        candidates = candidates | next_candidates
+
+    print(candidates.count())
+    print(candidates[0])
