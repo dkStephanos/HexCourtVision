f7ae4902809b822be81967cdee131ac9a4078a8a
dkStephanos <stephanos@goldmail.etsu.edu>
2021-02-23 18:13:18 -0500

    Added method to fit model and generate predictions

--- data/classification/ML/SVM.py
+++ data/classification/ML/SVM.py
@@ -1,4 +1,5 @@
 from sklearn import svm
+from sklearn.metrics import classification_report, confusion_matrix
 
 class SVM:
     def __init__(self,kernel):
@@ -6,3 +7,8 @@ class SVM:
 
     def get_model(self):
         return self.clf
+
+    def fit_and_predict(self, X_train, X_test, y_train):
+        self.clf.fit(X_train, y_train)
+        self.predictions = self.clf.predict(X_test)
+        
