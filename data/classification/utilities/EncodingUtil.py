6e7de1fcf6867cf76dc713d50705329f831aeaa9
dkStephanos <stephanos@goldmail.etsu.edu>
2021-02-23 17:51:03 -0500

    Added method to accept list of string tuple cols with positions, extract the tuple, sort by tuple and then encode in that order.

--- data/classification/utilities/EncodingUtil.py
+++ data/classification/utilities/EncodingUtil.py
@@ -9,3 +9,15 @@ class EncodingUtil:
             df[col] = le.fit_transform(df[col])
 
         return df
+
+    @staticmethod
+    def sort_position_cols_and_encode(df, cols):
+        le = preprocessing.LabelEncoder()
+
+        for col in cols:
+            # Construct the sort_val col from the position
+            df['sort_val'] = df[col].apply(lambda x: make_tuple(x))
+            df = df.sort_values('sort_val').drop('sort_val', 1)
+            df[col] = le.fit_transform(df[col])
+
+        return df
--- data/scripts/train_and_classify_models.py
+++ data/scripts/train_and_classify_models.py
@@ -12,4 +12,6 @@ def run():
     candidates_df.drop(columns=['candidate_id'], inplace=True)
 
     candidates_df = EncodingUtil.basic_label_encode_cols(candidates_df, ['cutter_archetype', 'screener_archetype'])
- 
+    candidates_df = EncodingUtil.sort_position_cols_and_encode(candidates_df, ['cutter_loc_on_pass'])
+
+    print(candidates_df['cutter_loc_on_pass'])
