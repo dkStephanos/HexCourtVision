acfeea228aa6b0dc9fa3a1f7a43e89d33b56d7fa
dkStephanos <stephanos@goldmail.etsu.edu>
2021-02-24 10:56:21 -0500

    Adding ML constantsutil, added col lists for encoding

--- data/scripts/train_and_classify_models.py
+++ data/scripts/train_and_classify_models.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from data.classification.utilities.EncodingUtil import EncodingUtil
+from data.classification.utilities.ConstantsUtil import ConstantsUtil
 from data.classification.ML.SVM import SVM
 
 from data.models import CandidateFeatureVector
@@ -13,23 +14,8 @@ def run():
     candidates_df.set_index('id', inplace=True)
     candidates_df.drop(columns=['candidate_id'], inplace=True)
 
-    candidates_df = EncodingUtil.basic_label_encode_cols(candidates_df, ['cutter_archetype', 'screener_archetype'])
-    candidates_df = EncodingUtil.sort_position_cols_and_encode(candidates_df, [ 'cutter_loc_on_pass',
-                                                                                'screener_loc_on_pass',
-                                                                                'ball_loc_on_pass',
-                                                                                'ball_radius_on_pass',
-                                                                                'cutter_loc_on_start_approach',
-                                                                                'screener_loc_on_start_approach',
-                                                                                'ball_loc_on_start_approach',
-                                                                                'ball_radius_loc_on_start_approach',
-                                                                                'cutter_loc_on_end_execution',
-                                                                                'screener_loc_on_end_execution',
-                                                                                'ball_loc_on_end_execution',
-                                                                                'ball_radius_loc_on_end_execution',
-                                                                                'cutter_loc_on_screen',
-                                                                                'screener_loc_on_screen',
-                                                                                'ball_loc_on_screen',
-                                                                                'ball_radius_on_screen'])
+    candidates_df = EncodingUtil.basic_label_encode_cols(candidates_df, ConstantsUtil.BASIC_ENCODE_COLS)
+    candidates_df = EncodingUtil.sort_position_cols_and_encode(candidates_df, ConstantsUtil.STRING_TUPLE_ENCODE_COLS)
 
     X = candidates_df.drop(columns=['classification'])
     y = candidates_df['classification']
