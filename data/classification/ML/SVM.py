3c83b1cbc4fc7f73f8c760ab83cb6ce8780603e8
dkStephanos <stephanos@goldmail.etsu.edu>
2021-02-26 09:48:10 -0500

    Made parent class for sklearn classification models, refactored SVM and functioning

--- data/classification/ML/SVM.py
+++ data/classification/ML/SVM.py
@@ -1,23 +1,8 @@
 from sklearn import svm, preprocessing
 from sklearn.metrics import classification_report, confusion_matrix
+from .SklearnClf import SklearnClf
 
-class SVM:
+class SVM(SklearnClf):
     def __init__(self, C, kernel="poly"):
+        super().__init__()
         self.clf = svm.SVC(C=C, kernel=kernel)
-        self.min_max_scaler = preprocessing.MinMaxScaler()
-
-    def get_model(self):
-        return self.clf
-
-    def fit_and_predict(self, X_train, X_test, y_train):      
-        X_train = self.min_max_scaler.fit_transform(X_train)
-        X_test = self.min_max_scaler.fit_transform(X_test)
-        
-        self.clf.fit(X_train, y_train)
-        self.predictions = self.clf.predict(X_test)
-
-    def get_confusion_matrix(self, y_test):
-        return confusion_matrix(y_test, self.predictions)
-
-    def get_classification_report(self, y_test):
-        return classification_report(y_test, self.predictions)
---
+++ data/classification/ML/SklearnClf.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import statistics as stats
+from sklearn import metrics, preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+
+
+class SklearnClf:
+    def __init__(self):
+        self.min_max_scaler = preprocessing.MinMaxScaler()
+
+    def get_model(self):
+        return self.clf
+
+    def fit_and_predict(self, X_train, X_test, y_train):      
+        X_train = self.min_max_scaler.fit_transform(X_train)
+        X_test = self.min_max_scaler.fit_transform(X_test)
+        
+        self.clf.fit(X_train, y_train)
+        self.predictions = self.clf.predict(X_test)
+
+    def split_test_data(self, df, test_size, target_col, is_fixed=False):
+        X = df.drop(columns=[target_col])
+        Y = df[target_col]
+        if(is_fixed):    #Use the same seed when generating test and training sets
+            X_train, X_test, Y_train, Y_test = train_test_split(X, Y, shuffle = True, random_state = 42, test_size = test_size)
+        else:           #Use a completely random set of test and training data
+            X_train, X_test, Y_train, Y_test = train_test_split(X, Y, shuffle = True, test_size = test_size)
+
+        return X_train, X_test, Y_train, Y_test
+
+    def get_confusion_matrix(self, y_test):
+        return confusion_matrix(y_test, self.predictions)
+
+    def get_classification_report(self, y_test):
+        return classification_report(y_test, self.predictions)
