d6d8b1b529618ddd9b38c7ce8999a381fbe1abf9
dkStephanos <stephanos@goldmail.etsu.edu>
2021-03-02 17:11:47 -0500

    Added set_data method to seperate test_data from dataset storage

--- data/classification/ML/SklearnClf.py
+++ data/classification/ML/SklearnClf.py
@@ -5,13 +5,16 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 
-
 class SklearnClf:
     def __init__(self):
         self.min_max_scaler = preprocessing.MinMaxScaler()
 
     def get_model(self):
         return self.clf
+
+    def set_data(self, df, target_col):
+        self.X = df.drop(columns=[target_col])
+        self.y = df[target_col]
 
     def get_data(self):
         return [self.X, self.y]
@@ -23,15 +26,15 @@ class SklearnClf:
         self.clf.fit(X_train, y_train)
         self.predictions = self.clf.predict(X_test)
 
-    def split_test_data(self, df, test_size, target_col, is_fixed=False):
-        self.X = df.drop(columns=[target_col])
-        self.y = df[target_col]
+    def split_test_data(self, test_size, is_fixed=False):
         if(is_fixed):    #Use the same seed when generating test and training sets
             X_train, X_test, Y_train, Y_test = train_test_split(self.X, self.y, shuffle = True, random_state = 42, test_size = test_size)
         else:           #Use a completely random set of test and training data
             X_train, X_test, Y_train, Y_test = train_test_split(self.X, self.y, shuffle = True, test_size = test_size)
 
         return X_train, X_test, Y_train, Y_test
+
+    
 
     def get_confusion_matrix(self, y_test):
         return confusion_matrix(y_test, self.predictions)
--- data/scripts/train_and_classify_gnb.py
+++ data/scripts/train_and_classify_gnb.py
@@ -17,7 +17,8 @@ def run():
     candidates_df = EncodingUtil.sort_position_cols_and_encode(candidates_df, ConstantsUtil.STRING_TUPLE_ENCODE_COLS)
 
     gnb = GaussianNB()
-    X_train, X_test, y_train, y_test = gnb.split_test_data(candidates_df, .2, 'classification', True)
+    gnb.set_data(candidates_df, 'classification')
+    X_train, X_test, y_train, y_test = gnb.split_test_data(.2, True)
     gnb.fit_and_predict(X_train, X_test, y_train)
     print(gnb.get_confusion_matrix(y_test))
     print(gnb.get_classification_report(y_test))
