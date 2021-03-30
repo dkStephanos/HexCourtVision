87e7abc9fc6341b7b9b20f0a8973396da8d8d8d2
dkStephanos <stephanos@goldmail.etsu.edu>
2021-03-30 17:42:13 -0400

    swapped to svm, works better, not great

--- data/scripts/test.py
+++ data/scripts/test.py
@@ -3,7 +3,7 @@ import pandas as pd
 from data.classification.utilities.EncodingUtil import EncodingUtil
 from data.classification.utilities.DataUtil import DataUtil
 from data.classification.utilities.ConstantsUtil import ConstantsUtil
-from data.classification.ML.NeuralNetwork import NeuralNetwork
+from data.classification.ML.SVM import SVM
 
 from data.models import CandidateFeatureVector
 from data.models import Candidate
@@ -16,16 +16,24 @@ def run():
     for candidate in candidates:
         player_id = Candidate.objects.values().get(candidate_id=candidate['candidate_id'])['player_a_id']
         team_id = Player.objects.values().get(player_id=player_id)['team_id']
-        if Team.objects.values().get(team_id=team_id)['abreviation'] == 'BOS':
+        if Team.objects.values().get(team_id=team_id)['abreviation'] == 'GSW':
             filtered_candidates.append(candidate)
 
     print("Success: ", len(candidates), len(filtered_candidates))
 
     candidates_df = DataUtil.get_candidates_df(filtered_candidates)
 
-    #NeuralNetwork.classify(candidates_df, .05, (512,256,128,64,32,16), 'relu', 'adam', .2, 'classification')
-    NeuralNetwork.plot_roc_curve(candidates_df, .05, (512,256,128,64,32,16), 'relu', 'adam', .2, 'classification')
-    #NeuralNetwork.plot_loss_val_curve(candidates_df, .05, (512,256,128,64,32,16), 'relu', 'adam', .2, 'classification')
+    svm = SVM(C=.75, kernel='poly')
+    svm.set_data(candidates_df, 'classification')
+    X_train, X_test, y_train, y_test = svm.split_test_data(.3, True)
+        
+    #svm.get_roc_curve(X_train, X_test, y_train, y_test)
 
-    results = NeuralNetwork.testNIterations(candidates_df, .05, (256,128,64,32,16), 'relu', 'adam', .3, 'classification', 10)
-    print(results)
+    svm.fit_and_predict(X_train, X_test, y_train)
+    print(svm.get_classification_report(y_test))
+
+    #svm.get_learning_curve()
+    #  Not working correctly... svm.get_validation_curve()
+
+    metrics = svm.get_avg_metrics_for_n_iterations(10, .3, True)
+    print(metrics)
