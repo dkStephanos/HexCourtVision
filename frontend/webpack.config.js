95be41391359f4fde1dde2609be53a45617cfac2
Koi Stephanos <dkstephanos@gmail.com>
2024-01-21 17:27:20 -0500

    updates for v5

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -8,31 +8,40 @@ module.exports = {
     filename: "[name].main.js"
   },
   resolve: {
-    modules: [path.resolve(__dirname, './'), 'node_modules']
+    modules: [path.resolve(__dirname, './'), 'node_modules'],
+    fallback: {
+      "buffer": require.resolve("buffer/"),
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "util": require.resolve("util/")
+    }
   },
   watch: true,
   module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
-        ]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!htmlparser2)/, // Exclude node_modules except htmlparser2
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'] // Ensure you have this preset installed
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ]
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
