87288737effe05ffbb87684e4df361ea8c09a2e5
Koi Stephanos <dkstephanos@gmail.com>
2024-01-30 22:30:45 -0500

    reset
    running in Node 18 with Next and simpler component kit

--- frontend/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import accountReducer from './accountReducer';
-
-const rootReducer = combineReducers({
-  account: accountReducer,
-});
-
-export default rootReducer;
