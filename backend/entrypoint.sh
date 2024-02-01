1381e7e606377cd7efad2b9af4663cc763e5dedd
Koi Stephanos <dkstephanos@gmail.com>
2024-01-31 23:14:24 -0500

    swapping migrations to entrypoint file

---
+++ backend/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Wait for database to be ready
+# Perform any other necessary checks
+until PGPASSWORD=$DATABASE_PASSWORD psql -h "$host" -U "$DATABASE_USER" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+# Apply migrations
+python ./backend/manage.py makemigrations
+python ./backend/manage.py migrate
+
+# Start the Django application
+exec python ./backend/manage.py runserver 0.0.0.0:8000
