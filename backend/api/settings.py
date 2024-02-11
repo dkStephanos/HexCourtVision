e480a74284397bb700d4095e8baf292b540a239a
Koi Stephanos <dkstephanos@gmail.com>
2024-02-11 15:12:31 -0500

    adding livereload

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'ml_nba',
     'django_extensions',
     'rest_framework',
+    'livereload',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'livereload.middleware.LiveReloadScript',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
