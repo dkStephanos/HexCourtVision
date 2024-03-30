cc517b06190081d7329c3dad1bba4e4f242d1d41
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-03-30 15:09:15 -0400

    working
    lots of progress. handling overwrite scenario currently

--- backend/ml_nba/preprocessing/persist_processed_game.py
+++ backend/ml_nba/preprocessing/persist_processed_game.py
@@ -2,9 +2,10 @@ from django.db import transaction
 from ml_nba.models import Game, Event, Moment, Candidate, Team, Player
 from ml_nba.preprocessing.utilities.DataLoader import DataLoader
 from ml_nba.preprocessing.utilities.DatabaseUtil import DatabaseUtil
+from ml_nba.preprocessing.utilities.EventsProcessor import EventsProcessor
 
 
-def persist_processed_game(game_id: str, overwrite: bool = True):
+def persist_processed_game(game_id: str, overwrite: bool = False):
     """
     Processes and persists all relevant data for a single NBA game into the database.
 
@@ -15,8 +16,8 @@ def persist_processed_game(game_id: str, overwrite: bool = True):
 
     Parameters:
     - game_id (str): A unique identifier for the game being processed.
-    - overwrite (bool): Indicates whether to overwrite previous game data. Defaults to true,
-                        and an exception is thrown if False and game data found
+    - overwrite (bool): Indicates whether to overwrite previous game data. Defaults to False.
+                        NOTE: An exception is thrown if False and game data found
 
     Steps involved:
     1. Load data files for the game, its events, combined event data, and candidate data.
@@ -32,19 +33,32 @@ def persist_processed_game(game_id: str, overwrite: bool = True):
     if DatabaseUtil.check_game_exists(game_id):
         if not overwrite:
             raise Exception(f"Cannot persist requested game_id: {game_id}. Game already present and overwrite set to False.")
+        else:
+            print('Overwrite set to True. Dropping previous entries...')
+            with transaction.atomic():
+                # Fetch related Event instances
+                game = Game.objects.get(game_id=game_id)
+                related_events = Event.objects.filter(game=game)
+                
+                # Delete related Moment/Candidate instances
+                Moment.objects.filter(event__in=related_events).delete()
+                Candidate.objects.filter(event__in=related_events).delete()
+                
+                # Now, you can delete the Event instances
+                related_events.delete()
     
-    print("Loading data files")
+    print("Loading data files...")
     game_df = DataLoader.load_raw_game(game_id)
     annotation_df = DataLoader.load_game_events(game_id)
     combined_event_df = DataLoader.load_processed_game(game_id)
     candidate_df = DataLoader.load_game_candidates(game_id)
 
-    print("Processing Data Files")
+    print("Processing Data Files...")
     game_data = DataLoader.get_game_data(game_df, annotation_df)
     teams_data = DataLoader.get_teams_data(game_df)
     players_data = DataLoader.get_players_data(game_df)
 
-    print("Creating or Updating Game/Team/Player models")
+    print("Creating or Updating Game/Team/Player models...")
     with transaction.atomic():
         home_team, _ = Team.objects.update_or_create(**teams_data["home_team"])
         visitor_team, _ = Team.objects.update_or_create(**teams_data["away_team"])
@@ -61,32 +75,86 @@ def persist_processed_game(game_id: str, overwrite: bool = True):
             Player,
             players_data,
             "player_id",
-            ["team_id", "last_name", "first_name", "jersey_number", "position"],
         )
 
-    print("Collecting and Creating Event and Moment Data")
-    events = []
-    moments = []
-    for index, event in combined_event_df.iterrows():
-        if (f"{event['GAME_ID']}-{event.index}" in set(candidate_df['event_id'])):
-            events.append(event)
-            moments.append(DataLoader.get_moments_from_event(event))
+    print("Collecting and Creating Event and Moment Models...")
+    event_instances = []
+    moment_instances = []
+    for _, event_row in combined_event_df.iterrows():
+        event_data = event_row.to_dict()
+        if (event_data['EVENT_ID'] in set(candidate_df['event_id'])):
+            # Map incoming event data to Event model fields
+            event_kwargs = {
+                'event_id': event_data['EVENT_ID'],
+                'game': game,
+                'possession_team': Team.objects.get(team_id=int(event_data['POSSESSION'])),
+                'player_1': Player.objects.filter(player_id=event_data['PLAYER1_ID']).first(),
+                'player_2': Player.objects.filter(player_id=event_data['PLAYER2_ID']).first(),
+                'player_3': Player.objects.filter(player_id=event_data['PLAYER3_ID']).first(),
+                'event_num': event_data['EVENT_ID'].split("-")[1],
+                'event_msg_type': event_data['EVENTMSGTYPE'],
+                'event_action_type': event_data['EVENTMSGACTIONTYPE'],
+                'period': event_data['PERIOD'],
+                'period_time': event_data['PCTIMESTRING'],
+                'home_desc': event_data.get('HOMEDESCRIPTION', ''),
+                'visitor_desc': event_data.get('VISITORDESCRIPTION', ''),
+                'score': event_data.get('SCORE', '0 - 0'),
+                'directionality': event_data.get('DIRECTION')
+            }
+            event = Event(**event_kwargs)
+            event_instances.append(event)
+
+            # Prepare Moment instances for bulk creation
+            for _, moment_row in EventsProcessor.get_moments_from_event(event_data).iterrows():
+                moment_data = moment_row.to_dict()
+                # Adjust moment_data to correctly reference related instances
+                adjusted_moment_data = {
+                    'team': Team.objects.filter(team_id=moment_data.get('team_id')).first(),
+                    'player': Player.objects.filter(player_id=moment_data.get('player_id')).first(),
+                    'event': event,  # Directly use the event instance created/updated above
+                    'x_loc': moment_data['x_loc'],
+                    'y_loc': moment_data['y_loc'],
+                    'radius': moment_data.get('radius', None),  # Provide a default if necessary
+                    'index': moment_data['index'],
+                    'game_clock': moment_data['game_clock'],
+                    'shot_clock': moment_data['shot_clock'],
+                }
+                
+                # Append a new Moment instance to the list for bulk creation
+                moment_instances.append(Moment(**adjusted_moment_data))
+    
+    print("Performing bulk inserts of generated models...")     
+    with transaction.atomic():
+        if event_instances:
+            Event.objects.bulk_create(event_instances)
+        if moment_instances:
+            Moment.objects.bulk_create(moment_instances)
+
+    print("Creating/Inserting Candidate Models...")
+    candidate_instances = []
+    for _, candidate_row in candidate_df.iterrows():
+        candidate_data = candidate_row.to_dict()
+        
+        # Prepare the kwargs for creating/updating the Candidate instance, mapping incoming data keys to model fields
+        candidate_kwargs = {
+            'candidate_id': candidate_data['candidate_id'],
+            'event': Event.objects.get(event_id=candidate_data['event_id']),
+            'classification_type': candidate_data['classification_type'],
+            'manual_label': candidate_data['manual_label'],
+            'period': candidate_data['period'],
+            'game_clock': candidate_data['game_clock'],
+            'shot_clock': candidate_data['shot_clock'],
+            'player_a': Player.objects.filter(player_id=candidate_data['player_a']).first() if 'player_a' in candidate_data else None,
+            'player_a_name': candidate_data.get('player_a_name', ''),
+            'player_b': Player.objects.filter(player_id=candidate_data['player_b']).first() if 'player_b' in candidate_data else None,
+            'player_b_name': candidate_data.get('player_b_name', ''),
+            'notes': candidate_data.get('notes', '')
+        }
+        
+        candidate_instances.append(Candidate(**candidate_kwargs))
 
     with transaction.atomic():
-        for event_data in events:
-            event, _ = Event.objects.get_or_create(**event_data)
+        if candidate_instances:
+            Candidate.objects.bulk_create(candidate_instances)
 
-            # Assume moments_data is structured for bulk creation
-            Moment.objects.bulk_create(
-                [
-                    Moment(**moment_data, event=event)
-                    for moment_data in moments[event_data["event_id"]]
-                ]
-            )
-
-    print("Creating Candidate Models")
-    with transaction.atomic():
-        for candidate_data in candidate_df.itertuples(index=False):
-            Candidate.objects.get_or_create(**dict(candidate_data))
-
-    print("Finished processing game")
+    print("Finished processing game.")
