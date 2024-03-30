b6a310f1fd6f3b1cdbb530ffef756f964424c16e
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-03-30 15:50:37 -0400

    refactoring

--- backend/ml_nba/preprocessing/generate_dho_feature_vectors.py
+++ backend/ml_nba/preprocessing/generate_dho_feature_vectors.py
@@ -1,38 +1,32 @@
 import sys, os, traceback
-import pandas as pd
 from ml_nba.models import Game, Event, Candidate, CandidateFeatureVector
 from ml_nba.preprocessing.utilities.FeatureUtil import FeatureUtil
 
 
-pd.set_option('mode.chained_assignment', None)
-
-def generate_dho_feature_vectors():
+def generate_dho_feature_vectors(game_key):
     num_failed_candidates = 0
     num_successful_candidates = 0
     issue_candidates = []
-    for game in Game.objects.all():
-        events = Event.objects.filter(game=game)
-        for event in events:
-            next_candidates = Candidate.objects.filter(event=event).values()
-            for target_candidate in next_candidates:
-                has_vector = CandidateFeatureVector.objects.filter(candidate=target_candidate).exists()
-                if not has_vector:
-                    try:
-                        vector = FeatureUtil.generate_dribble_handoff_feature_vector(target_candidate)
-                        CandidateFeatureVector.objects.update_or_create(**vector)
-                        num_successful_candidates += 1
-                    except Exception as e:
-                        print(f"Issue at candidate: {target_candidate['candidate_id']}")
-                        exc_type, exc_obj, exc_tb = sys.exc_info()
-                        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                        print(exc_type, fname)
-                        print(traceback.print_tb(exc_tb))
-                        issue_candidates.append(target_candidate)
-                        num_failed_candidates += 1
+    game = Game.objects.get(game_id=game_key)
+    events = Event.objects.filter(game=game)
+    for event in events:
+        next_candidates = Candidate.objects.filter(event=event).values()
+        for target_candidate in next_candidates:
+            has_vector = CandidateFeatureVector.objects.filter(candidate=target_candidate).exists()
+            if not has_vector:
+                try:
+                    vector = FeatureUtil.generate_dribble_handoff_feature_vector(target_candidate)
+                    CandidateFeatureVector.objects.update_or_create(**vector)
+                    num_successful_candidates += 1
+                except Exception as e:
+                    print(f"Issue at candidate: {target_candidate['candidate_id']}")
+                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                    print(exc_type, fname)
+                    print(traceback.print_tb(exc_tb))
+                    issue_candidates.append(target_candidate)
+                    num_failed_candidates += 1
 
-        output = f"Total successful candidates: {num_successful_candidates}\nTotal failed candidates: {num_failed_candidates}" 
-        print(output)
-        text_file = open("static/backend/test/feature_gen_results_round2.txt", "w")
-        text_file.write(output)
-        text_file.close()
+    output = f"Total successful candidates: {num_successful_candidates}\nTotal failed candidates: {num_failed_candidates}" 
+    print(output)
     
