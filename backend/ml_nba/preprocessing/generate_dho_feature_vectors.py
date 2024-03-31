45301a25d9ea01caa714f5148e609e4218df8571
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-03-31 11:07:51 -0400

    adding transaction.atomic() contextmngr

--- backend/ml_nba/preprocessing/generate_dho_feature_vectors.py
+++ backend/ml_nba/preprocessing/generate_dho_feature_vectors.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from ml_nba.models import Game, Event, Candidate, CandidateFeatureVector
 from ml_nba.preprocessing.utilities.FeatureUtil import FeatureUtil
 
@@ -6,9 +7,11 @@ def generate_dho_feature_vectors(game_key):
     game = Game.objects.get(game_id=game_key)
     events = Event.objects.filter(game=game)
     
-    for event in events:
-        candidates = Candidate.objects.filter(event=event).values()
-        for target_candidate in candidates:
-            vector = FeatureUtil.generate_dribble_handoff_feature_vector(target_candidate)
-            CandidateFeatureVector.objects.update_or_create(**vector)
+    with transaction.atomic():
+        for event in events:
+            candidates = Candidate.objects.filter(event=event).values()
+            for target_candidate in candidates:
+                vector = FeatureUtil.generate_dribble_handoff_feature_vector(target_candidate)
+                print(vector)
+                CandidateFeatureVector.objects.update_or_create(**vector)
 
