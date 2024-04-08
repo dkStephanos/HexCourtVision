9cb72f0e4e90195a9fb6b5a6027bf626ffc0e79e
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-04-07 21:45:13 -0400

    setting up startup script for notebooks env

---
+++ backend/notebooks/startup.py
@@ -0,0 +1,10 @@
+# startup.py
+import matplotlib
+# Set Matplotlib to display figures inline in the IPython notebook
+matplotlib.use('module://ipykernel.pylab.backend_inline')
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+print("Common libraries loaded.")
